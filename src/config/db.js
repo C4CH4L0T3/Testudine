a8b1fdb2c47969c3e1d757100688eff9fe030e05
Emmanuel Moreno Alvarez <83464263+C4CH4L0T3@users.noreply.github.com>
2022-10-21 13:55:11 -0500

    push final manual

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'Testudine',
+    database: 'testudine',
     waitForConnections: true
 });
 
